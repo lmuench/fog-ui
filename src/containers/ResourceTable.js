ef9a1b3c6c5b4a41ffe09c6650243083580ebdca
Ludwig Muench <muench.ludwig@gmail.com>
2018-10-06 12:28:09 +0200

    change sematable columns

--- src/containers/ResourceTable.js
+++ src/containers/ResourceTable.js
@@ -5,25 +5,31 @@ import EditableCell from './EditableCell.js';
 export const RESOURCE_TABLE = 'resourceTable';
 
 const columns = [
-  { key: 'id', primaryKey: true, header: 'ID', sortable: true },
-  { key: 'name', header: 'Name', searchable: true, sortable: true },
-  { key: 'path', header: 'Path', sortable: true, Component: EditableCell },
+  { key: 'add', header: 'Add', sortable: true },
   {
-    key: 'type',
-    header: 'Type',
+    key: 'status',
+    header: 'status',
     filterable: true,
     filterValues: [
-      'ACTUATOR',
-      'SENSOR',
-      'SENSOR:SMOKE',
-      'SENSOR:TEMPERATURE',
+      'NEW',
+      'INCLUDED',
+      'DEAD',
     ],
     getFilterClassName: value => `col-${value.toLowerCase()}`,
     sortable: true
   },
+  { key: 'customPath', header: 'Custom Path', searchable: true, sortable: true, Component: EditableCell },
+  { key: 'originalPath', primaryKey: true, header: 'Original Path', searchable: true, sortable: true },
+  { key: 'rt', header: 'Resource Type', searchable: true, sortable: true },
+  { key: 'if', header: 'Interface Description', searchable: true, sortable: true },
+  { key: 'protocol', header: 'Protocol', sortable: true },
+  { key: 'ep', header: 'Endpoint', searchable: true, sortable: true },
+  { key: 'd', header: 'Sector', searchable: true, sortable: true },
+  { key: 'base', header: 'Base', searchable: true, sortable: true },
+  { key: 'gp', header: 'Group', searchable: true, sortable: true }  
 ];
 
-class ResourceTable extends Component {
+class  ResourceTable extends Component {
   render = () => {
     return <Table {...this.props} columns={columns} />;
   }
