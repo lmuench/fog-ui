3adbb3b3b1bbb82523c08dbc6fbe103b5cc2eb4d
Ludwig Muench <muench.ludwig@gmail.com>
2018-10-06 14:14:34 +0200

    add navbar link to felix web console

--- src/components/misc/TopBar.js
+++ src/components/misc/TopBar.js
@@ -8,22 +8,28 @@ const TopBar = props => (
     <Navbar>
       <Navbar.Header>
         <Navbar.Brand>
-          <a href="#home">React-Bootstrap</a>
+          <Link to={'/'}>Fog Node</Link>
         </Navbar.Brand>
       </Navbar.Header>
       <Nav>
-        <NavItem eventKey={1} href="#">
-          Link
+        <NavItem
+          eventKey={1}
+          componentClass={Link}
+          href="/apibuilder"
+          to="/apibuilder"
+          active={window.location.hash === '#/apibuilder'}
+        >
+          API Builder
         </NavItem>
-         <NavItem eventKey={2} href="#">
-            Link
+        <NavItem eventKey={2} href="http://localhost:8080/system/console/bundles">
+          Felix Web Console
         </NavItem>
-        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-          <MenuItem eventKey={3.1}>Action</MenuItem>
-          <MenuItem eventKey={3.2}>Another action</MenuItem>
-          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+        <NavDropdown eventKey={2} title="Dropdown" id="basic-nav-dropdown">
+          <MenuItem eventKey={2.1}>Action</MenuItem>
+          <MenuItem eventKey={2.2}>Another action</MenuItem>
+          <MenuItem eventKey={2.3}>Something else here</MenuItem>
           <MenuItem divider />
-          <MenuItem eventKey={3.4}>Separated link</MenuItem>
+          <MenuItem eventKey={2.4}>Separated link</MenuItem>
         </NavDropdown>
       </Nav>
     </Navbar>
