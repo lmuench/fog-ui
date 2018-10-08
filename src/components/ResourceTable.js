a9e3cd3bdb36f7091086b4c5b1a5374e28eb8c99
Ludwig Muench <muench.ludwig@gmail.com>
2018-10-08 19:42:49 +0200

    re-arrange columns

--- src/components/ResourceTable.js
+++ src/components/ResourceTable.js
@@ -7,28 +7,28 @@ const CustomPathCell = props => (
 );
 
 const columns = [
-  { key: 'add', header: 'Add', sortable: true },
-  {
-    key: 'status',
-    header: 'status',
-    filterable: true,
-    filterValues: [
-      'NEW',
-      'INCLUDED',
-      'DEAD',
-    ],
-    getFilterClassName: value => `col-${value.toLowerCase()}`,
-    sortable: true
-  },
-  { key: 'customPath', header: 'Custom Path', searchable: true, sortable: true, Component: CustomPathCell },
+  // TODO: status
+  // {
+  //   key: 'status',
+  //   header: 'status',
+  //   filterable: true,
+  //   filterValues: [
+  //     'NEW',
+  //     'INCLUDED',
+  //     'DEAD',
+  //   ],
+  //   getFilterClassName: value => `col-${value.toLowerCase()}`,
+  //   sortable: true
+  // },
+  { key: 'base', header: 'Base URI', searchable: true, sortable: true },
   { key: 'path', primaryKey: true, header: 'Original Path', searchable: true, sortable: true },
+  { key: 'customPath', header: 'Custom Path', searchable: true, sortable: true, Component: CustomPathCell },
   { key: 'rt', header: 'Resource Type', searchable: true, sortable: true },
   { key: 'if', header: 'Interface Description', searchable: true, sortable: true },
-  { key: 'protocol', header: 'Protocol', sortable: true },
   { key: 'ep', header: 'Endpoint', searchable: true, sortable: true },
   { key: 'd', header: 'Sector', searchable: true, sortable: true },
-  { key: 'base', header: 'Base URI', searchable: true, sortable: true },
-  { key: 'gp', header: 'Group', searchable: true, sortable: true }
+  { key: 'protocol', header: 'Protocol', sortable: true },
+  // { key: 'gp', header: 'Group', searchable: true, sortable: true }
 ];
 
 class  ResourceTable extends Component {
